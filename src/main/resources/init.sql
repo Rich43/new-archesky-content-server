e8233f65376ad2dc59ff87c89bcb12026f564e75
Richard Ward <pynguins@outlook.com>
2021-04-17 12:37:59 +0100

    Add init script

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS content;
-CREATE USER 'content'@'localhost' IDENTIFIED BY 'Password1';
+CREATE USER IF NOT EXISTS 'content'@'localhost' IDENTIFIED BY 'Password1';
 GRANT SELECT, INSERT, UPDATE, DELETE ON content TO 'content'@'localhost';
