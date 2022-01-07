2af126fba29134e71058f339c246ba784e733f7b
Richard Ward <richies@gmail.com>
2022-01-07 16:21:03 +0000

    Add PreAuthorise

--- src/main/java/com/archesky/content/repository/ContentRepository.java
+++ src/main/java/com/archesky/content/repository/ContentRepository.java
@@ -4,6 +4,7 @@ import com.archesky.content.entities.Content;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.Optional;
 
@@ -11,4 +12,32 @@ import java.util.Optional;
 public interface ContentRepository extends PagingAndSortingRepository<Content, Integer> {
     @RestResource(rel = "by-name")
     Optional<Content> findByName(String name);
+
+    @PreAuthorize("hasAuthority('archesky.create_content')")
+    @Override
+    <S extends Content> S save(S entity);
+
+    @PreAuthorize("hasAuthority('archesky.create_content')")
+    @Override
+    <S extends Content> Iterable<S> saveAll(Iterable<S> entities);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content')")
+    @Override
+    void deleteById(Integer integer);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content')")
+    @Override
+    void delete(Content entity);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content')")
+    @Override
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content')")
+    @Override
+    void deleteAll(Iterable<? extends Content> entities);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content')")
+    @Override
+    void deleteAll();
 }
--- src/main/java/com/archesky/content/repository/ContentRevisionRepository.java
+++ src/main/java/com/archesky/content/repository/ContentRevisionRepository.java
@@ -3,7 +3,35 @@ package com.archesky.content.repository;
 import com.archesky.content.entities.ContentRevision;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource(collectionResourceRel = "contentRevision", path = "contentRevision")
 public interface ContentRevisionRepository extends PagingAndSortingRepository<ContentRevision, Integer> {
+    @PreAuthorize("hasAuthority('archesky.create_content_revision')")
+    @Override
+    <S extends ContentRevision> S save(S entity);
+
+    @PreAuthorize("hasAuthority('archesky.create_content_revision')")
+    @Override
+    <S extends ContentRevision> Iterable<S> saveAll(Iterable<S> entities);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content_revision')")
+    @Override
+    void deleteById(Integer integer);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content_revision')")
+    @Override
+    void delete(ContentRevision entity);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content_revision')")
+    @Override
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content_revision')")
+    @Override
+    void deleteAll(Iterable<? extends ContentRevision> entities);
+
+    @PreAuthorize("hasAuthority('archesky.delete_content_revision')")
+    @Override
+    void deleteAll();
 }
